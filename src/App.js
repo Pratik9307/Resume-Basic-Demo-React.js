95d560994827a31cc6dc020fc4732dc78a01653b
Pratik9307 <pratiksatalkar01@gmail.com>
2024-07-01 11:13:55 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,100 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       <div className='App'>
+           <div className='left-side'>  
+               <div className='Profile'>
+                  <div className='imgBx'> 
+                    <img src="C:\Users\user nc\Desktop\img.jpg.jpg"></img>
+                  </div>
+                      <br/>
+                      <br/>
+                      <li> Phone:- +91 9307132146 </li><br/>
+                      <li> pratiksatalkar1@gmail.com </li> <br/>
+                      <li> Kopargoan,Maharashtra</li> 
+                  
+                      <div >
+                      <h2 id="id1"> EDUCATION</h2>
+                      <li> <b>SSC Grade</b>  - 80.20% <br/>
+                       state  board of maharashtra   <br/>
+                        2018-19
+                      <br/> 
+                      </li>
+                      <br/>
+                      <li> <b>Hsc Grade </b> Hsc Grade - 75.87% <br/>
+                      state  board of maharashtra   <br/>
+                      2018-19
+                      <br/> 
+                      </li>
+                      <div> 
+                      <h2 id="id2"> EXPERTISE</h2>
+                      <li> Web Design </li><br/>
+                      <li> Python</li><br/>
+                      <li> Cyber Security</li><br/>
+                      <li> Basic knowledge of SQL </li><br/>
+                      <li> Html </li><br/>
+                      <li> css </li><br/>
+                      <li> react.js </li><br/>
+                      </div>
+                      <div>
+
+                      <h2 id="id3"> LANGUAGE</h2>
+                      <li> English</li><br/>
+                      <li> Hindi</li><br/>
+                     
+                      </div>   
+                   </div>                  
+               </div>
+           </div>
+           
+           <div className='Right-side'>                 
+                 <div className="class1"> <b> Pratik Satalkar </b><br/>
+                    <span id="Id4"> Mechatronics Engineering</span>
+                    <b><hr/></b>
+                 </div>
+           <div>     
+                    <h2 id="Id5">ABOUT ME <br/></h2> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;<span Id="Don">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; looking  for opportunities 
+                    to incorporate my skill and training to help the company grow,
+                    i am looking forward to roles that will help me realize my potential 
+                    by exploring the various aspect of this field
+                    </span>
+                   
+                    <div>
+                    <h2 id="st"> PROJECTS</h2>
+                    <h4> <li> ➢ Cup Washer Using Sensor</li></h4>
+                    <span> In this project we have made a product at very 
+                    low price and for use of many hotels working people that 
+                    they can buy and proceed their work without any problem.</span>
+                    <h4> <li> ➢ Solar Panel Cleaner System  using ESP32 
+                     microcontrollers </li></h4>
+                     <span>(This project is based on IOT system which is used for cleaning
+                      solar panel without no any harassment and It is a  Based onreal life Problem) </span>
+                    </div>
+                    <div><h2 id="pt">ACHIEVEMENTS</h2> 
+                    <span>  
+                     ➢ To secure the 2nd rank in the IEEE competition among 100 groups, 
+                     our team demonstrated unparalleled innovation and technical expertise,
+                      distinguishing ourselves as true leaders in the field of Mechatronics Engineering.
+
+                    </span><br/>
+                    <br/>
+                    <span>➢ Selected for DIPEX 2023, a National  level Project Competition</span> <br/>
+                    <br/>
+                    <span> ➢I am participating in the Akruti competition, a design contest organized by Desolt Systems.</span><br/>
+                    <br/>
+                    </div>
+                    <div>
+                    <h2 id="pst"> CERTIFICATION</h2>
+                    <span>➢ NPTEL (python for data science)</span> <br/>
+                    <br/>
+                    <span>➢ Cyber Threat Intelligence ( Coursera)</span> <br/>
+
+                    </div>
+                </div>
+              </div>
+           </div>
   );
 }
 
